a60b4a4d8fbebdb24f3a86fa4365187e501e60d4
efvitoly <enforevitoly@gmail.com>
2025-08-03 20:50:58 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,1 @@
-// components/Footer.tsx
-import React from 'react';
 
-const Footer: React.FC = () => {
-  return (
-    <footer style={{
-      padding: '1rem 2rem',
-      backgroundColor: '#222',
-      color: 'white',
-      textAlign: 'center',
-      marginTop: 'auto'
-    }}>
-      <p>© 2025 My Next.js App. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
